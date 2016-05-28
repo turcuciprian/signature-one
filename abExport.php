c55e0eb46ee9ed58593c9b51666aae9c77d8d722
Ciprian Turcu <turcuciprian1@gmail.com>
2016-05-29 00:21:13 +0300

    udpated readme

--- abExport.php
+++ abExport.php
@@ -21,7 +21,7 @@
                   if ($pluginInstalled) {
                       //check if plugin is activated:
                               if (!is_plugin_active('admin-builder/admin_builder.php')) {
-                                  $abError = true;
+                                $abError = true;
                                   $url = admin_url();
 
                                   echo '<div class="notice notice-error is-dismissible">';
@@ -31,35 +31,37 @@
                                   echo '</p>';
                                   echo '</div>';
             //plugin is activated
-                              } else {
-                                  $theJson = '{"menus":[{"label":"Posts","type":"post","name":"posts","unique":true,"children":[{"label":"Metabox1","name":"metabox1","context":"normal","priority":"default","fields":[{"name":"textbox1","type":"textbox","label":"TextBox1","description":"Default TextBox Description text","$$hashKey":"object:550"}],"$$hashKey":"object:477"}],"$$hashKey":"object:17"},{"label":"Pages","type":"page","name":"pages","unique":true,"children":[{"label":"Metabox1","name":"metabox1","context":"normal","priority":"high","fields":[{"name":"textbox1","type":"textbox","label":"TextBox1","description":"Default TextBox Description text","$$hashKey":"object:602"}],"$$hashKey":"object:529"}],"$$hashKey":"object:18"},{"label":"Admin Page1","type":"cPage","name":"cPage1","unique":false,"children":[{"label":"Tab1","name":"tab1","context":"normal","priority":"default","fields":[{"name":"textbox1","type":"textbox","label":"TextBox1","description":"Default TextBox Description text","$$hashKey":"object:335"},{"name":"textarea1","type":"textarea","label":"TextArea1","description":"Default TextArea Description text","$$hashKey":"object:446"},{"name":"select1","type":"select","label":"Select1","description":"Default Select Description text","selectType":"custom","oArr":[],"$$hashKey":"object:537"},{"name":"radio1","type":"radio","label":"Radio Buttons1","description":"Default Radio Buttons Description text","oArr":[],"radioType":"custom","orientation":"h","$$hashKey":"object:628"}],"$$hashKey":"object:75"}],"capability":"manage_options","handler":"ab_menu_handler1","pageTitle":"Test CPage","$$hashKey":"object:72","pageDescription":"HEre\'s some description. What you need to do here is listen. and listen good!"}]}';
-                                  $lfp = new loadFromPlugin();
-                                  $lfp->load($theJson);
-                              }
-                  } else {
-                      $abError = true;
-                      echo '<div class="notice notice-error is-dismissible">';
-                      echo '<h3>Admin Builder Plugin is not installed!</h3>';
-                      echo '<p>';
-                      echo 'To get the full functionality , install Admin Builder.';
-                      echo '</p>';
-                      echo '<p>';
-                      $plugin_name = 'admin-builder';
-                      $install_link = '<a href="'.esc_url(network_admin_url('plugin-install.php?tab=plugin-information&amp;plugin='.$plugin_name.'&amp;TB_iframe=true&amp;width=600&amp;height=550')).'" class="thickbox" title="More info about '.$plugin_name.'">Install '.$plugin_name.'</a>';
-
-                      echo $install_link;
-                      echo '</p>';
-                      echo '</div>';
-                  }
-              }
-              add_action('admin_notices', 'sample_admin_notice__success');
-          }
-      if (!$abError) {
-          if (!$abError) {
-              if (class_exists(loadFromPlugin)) {
-                  $theJson = '{"menus":[{"label":"Posts","type":"post","name":"posts","unique":true,"children":[],"$$hashKey":"object:4"},{"label":"Pages","type":"page","name":"pages","unique":true,"children":[],"$$hashKey":"object:5"},{"label":"Signature One","type":"cPage","name":"signature_one","unique":false,"children":[{"label":"General","name":"tab1","context":"normal","priority":"default","fields":[{"name":"textbox1","type":"textbox","label":"Signature Text","description":"The default signature Text","$$hashKey":"object:503"},{"name":"select1","type":"select","label":"Font Size","description":"Select a font size for the signature from the available list of options above","selectType":"custom","oArr":[{"label":"Small (14 px)","value":"14","$$hashKey":"object:705"},{"label":"Medium (18px)","value":"18","$$hashKey":"object:716"},{"label":"Large (22 px)","value":"22","$$hashKey":"object:725"},{"label":"Huge (32 px)","value":"32","$$hashKey":"object:734"},{"label":"Giant (46 px)","value":"46","$$hashKey":"object:845"}],"$$hashKey":"object:614"}],"$$hashKey":"object:35"},{"label":"Defaults","name":"tab2","context":"normal","priority":"default","fields":[{"name":"checkbox1","type":"checkbox","label":"Default for Posts","description":"All across the site posts will be affected (the signature will be added.","extraText":"Check if you want to affect posts by Default","$$hashKey":"object:447"},{"name":"checkbox2","type":"checkbox","label":"Default for Pages","description":"All across the site pages will be affected (the signature will be added.","extraText":"Check if you want to affect pages by Default","$$hashKey":"object:538"}],"$$hashKey":"object:424"}],"capability":"manage_options","handler":"soHandler","pageTitle":"Global Settings","$$hashKey":"object:29","pageDescription":"The settings that apply to all posts and pages , as defaults. You are able to customize everything"}]}';
-                  $lfp = new loadFromPlugin();
-                  $lfp->load($theJson);
-              }
-          }
+          }else{
+      $theJson = '{"menus":[{"label":"Posts","type":"post","name":"posts","unique":true,"children":[{"label":"Metabox1","name":"metabox1","context":"normal","priority":"default","fields":[{"name":"textbox1","type":"textbox","label":"TextBox1","description":"Default TextBox Description text","$$hashKey":"object:550"}],"$$hashKey":"object:477"}],"$$hashKey":"object:17"},{"label":"Pages","type":"page","name":"pages","unique":true,"children":[{"label":"Metabox1","name":"metabox1","context":"normal","priority":"high","fields":[{"name":"textbox1","type":"textbox","label":"TextBox1","description":"Default TextBox Description text","$$hashKey":"object:602"}],"$$hashKey":"object:529"}],"$$hashKey":"object:18"},{"label":"Admin Page1","type":"cPage","name":"cPage1","unique":false,"children":[{"label":"Tab1","name":"tab1","context":"normal","priority":"default","fields":[{"name":"textbox1","type":"textbox","label":"TextBox1","description":"Default TextBox Description text","$$hashKey":"object:335"},{"name":"textarea1","type":"textarea","label":"TextArea1","description":"Default TextArea Description text","$$hashKey":"object:446"},{"name":"select1","type":"select","label":"Select1","description":"Default Select Description text","selectType":"custom","oArr":[],"$$hashKey":"object:537"},{"name":"radio1","type":"radio","label":"Radio Buttons1","description":"Default Radio Buttons Description text","oArr":[],"radioType":"custom","orientation":"h","$$hashKey":"object:628"}],"$$hashKey":"object:75"}],"capability":"manage_options","handler":"ab_menu_handler1","pageTitle":"Test CPage","$$hashKey":"object:72","pageDescription":"HEre\'s some description. What you need to do here is listen. and listen good!"}]}';
+      $lfp = new loadFromPlugin();
+      $lfp->load($theJson);
       }
+
+              } else {
+                $abError = true;
+                  echo '<div class="notice notice-error is-dismissible">';
+                  echo '<h3>Admin Builder Plugin is not installed!</h3>';
+                  echo '<p>';
+                  echo 'To get the full functionality , install Admin Builder.';
+                  echo '</p>';
+                  echo '<p>';
+                  $plugin_name = 'admin-builder';
+                  $install_link = '<a href="'.esc_url(network_admin_url('plugin-install.php?tab=plugin-information&amp;plugin='.$plugin_name.'&amp;TB_iframe=true&amp;width=600&amp;height=550')).'" class="thickbox" title="More info about '.$plugin_name.'">Install '.$plugin_name.'</a>';
+
+                  echo $install_link;
+                  echo '</p>';
+                  echo '</div>';
+              }
+          }
+          add_action('admin_notices', 'sample_admin_notice__success');
+      }
+      if(!$abError){
+        if (!$abError) {
+            if (class_exists('loadFromPlugin')) {
+
+$theJson = '{"menus":[{"label":"Posts","type":"post","name":"posts","unique":true,"children":[{"label":"Signature One Settings","name":"metabox1","context":"normal","priority":"default","fields":[{"name":"textbox1","type":"textbox","label":"Signature Text","description":"Enter the text you want as a signature to Show for this specific Post. (Default Global Options will be applied if this is left empty)","$$hashKey":"object:744"},{"name":"select1","type":"select","label":"Size","description":"The size of the signature text","selectType":"custom","oArr":[{"label":"Global Default","value":"0","$$hashKey":"object:5201"},{"label":"Small (14 px)","value":"14","$$hashKey":"object:5212"},{"label":"Medium (18px)","value":"18","$$hashKey":"object:6064"},{"label":"Large (22 px)","value":"22","$$hashKey":"object:6073"},{"label":"Huge (32 px)","value":"32","$$hashKey":"object:6082"},{"label":"Giant (46 px)","value":"46","$$hashKey":"object:532"}],"$$hashKey":"object:5110"},{"name":"select2","type":"select","label":"Signature Position","description":"The signature will display on the bottom of each post. What side, you decide.","selectType":"custom","oArr":[{"label":"Globally Default","value":"0","$$hashKey":"object:1010"},{"label":"Center","value":"center","$$hashKey":"object:1021"},{"label":"Left","value":"left","$$hashKey":"object:1030"},{"label":"Right","value":"right","$$hashKey":"object:491"}],"$$hashKey":"object:919"}],"$$hashKey":"object:668"}],"$$hashKey":"object:4"},{"label":"Pages","type":"page","name":"pages","unique":true,"children":[],"$$hashKey":"object:5"},{"label":"Signature One","type":"cPage","name":"signature_one","unique":false,"children":[{"label":"Single Page Content","name":"tab1","context":"normal","priority":"default","fields":[{"name":"select1","type":"select","label":"Size","description":"Select asize for the signature from the available list of options above","selectType":"custom","oArr":[{"label":"Small (14 px)","value":"14","$$hashKey":"object:705"},{"label":"Medium (18px)","value":"18","$$hashKey":"object:716"},{"label":"Large (22 px)","value":"22","$$hashKey":"object:725"},{"label":"Huge (32 px)","value":"32","$$hashKey":"object:734"},{"label":"Giant (46 px)","value":"46","$$hashKey":"object:845"}],"$$hashKey":"object:614"},{"name":"textbox1","type":"textbox","label":"Signature Text","description":"The default signature Text","$$hashKey":"object:503"},{"name":"select2","type":"select","label":"Signature Position","description":"The signature will display on the bottom of each post. What side, you decide.","selectType":"custom","oArr":[{"label":"Center (default)","value":"center","$$hashKey":"object:585"},{"label":"Left","value":"left","$$hashKey":"object:596"},{"label":"Right","value":"right","$$hashKey":"object:605"}],"$$hashKey":"object:491"}],"$$hashKey":"object:35"}],"capability":"manage_options","handler":"soHandler","pageTitle":"Global Settings","$$hashKey":"object:29","pageDescription":"The settings that apply to all posts and pages , as defaults. You are able to customize everything"}]}';
+$lfp = new loadFromPlugin();
+$lfp->load($theJson);
+ }
+}
+}
--- signature-one.php
+++ signature-one.php
@@ -10,4 +10,43 @@
   Text Domain: Signature-one
  */
 
- require_once ("abExport.php");
+ require_once 'abExport.php';
+
+ add_filter('the_content', 'so_the_content_callback');
+ function so_the_content_callback($content)
+ {
+     global $abGen;
+     if (is_single()) {
+         //general values
+         $GlobalSignature = $abGen->getField('abOption_cPage_signature_one', 'tab1', 'textbox1');
+         $globalPosition = $abGen->getField('abOption_cPage_signature_one', 'tab1', 'select2');
+         $globalSize = $abGen->getField('abOption_cPage_signature_one','tab1','select1');
+
+         //post specific values:
+         $postSignature = get_post_meta(get_the_ID(), 'abMB_metabox1textbox1', true);
+         $postPosition = get_post_meta(get_the_ID(),"abMB_metabox1select2",true);
+         $postSize = get_post_meta(get_the_ID(),"abMB_metabox1select1",true);
+
+         $fSignature = $GlobalSignature;
+         $fPosition = $globalPosition;
+         $fSize = $globalSize;
+
+         if(!empty($postSignature)){
+           $fSignature = $postSignature;
+         }
+         if(!empty($postSize) && $postSize!="0"){
+           $fSize = $postSize;
+         }
+         if(!empty($postPosition) && $postPosition!="0"){
+           $fPosition = $postPosition;
+         }
+
+         // decisions decisions
+
+         $soSignature = $GlobalSignature;
+         $SignatureHTML = '<div class="soContainer" style="display:block;text-align:'.$fPosition.';font-size:'.$fSize.'px;">'.$fSignature.'</div>';
+         $content = $content.$SignatureHTML;
+     }
+
+     return $content;
+ }
